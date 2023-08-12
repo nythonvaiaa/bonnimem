41748a59dd5e1b83bb7da6d94d4cf2be26dadde7
Code With Noman <85833961+nythonvaiaa@users.noreply.github.com>
2023-08-13 05:37:20 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('click').addEventListener('click',function(){
+    document.getElementById('first').classList.add('d-none');
+    document.getElementById('newYear').classList.add('d-block')
+})
